c6e08c3353cce380d998a70b773a4cebfa6ef8f8
Aqif Sariman <79038951+aqifsariman@users.noreply.github.com>
2022-08-08 09:21:49 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
 
 const PORT = process.env.PORT || 3004;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello there! Welcome to the server!' });
